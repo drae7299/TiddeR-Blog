6e2057e11e5ad2d26edf662bb25c7a5abb758488
Brandon <hutchingsonb@gmail.com>
2021-04-30 11:31:24 -0400

    added front end logic for new user signup

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,29 @@
-const test = 5;
+//get signup form id's
+const name = document.querySelector('#name-signup').value.trim();
+const username = document.querySelector('#username-signup').value.trim();
+const email = document.querySelector('#email-signup').value.trim();
+const password = document.querySelector('#password-signup').value.trim();
+
+//function for signing up
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to sign up.');
+      }
+    }
+};
+
+//get submit button for user sign up
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
