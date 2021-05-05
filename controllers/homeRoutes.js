e02051a645e4317bc22d1d7f280da838602f3be5
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-05-04 22:02:10 -0400

    added individual blog route - includes comments on that blog post (not formatted yet)

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -96,6 +96,33 @@ router.get('/channel/:id', withAuth, async (req, res) => {
   }
 });
 
+// show one blog - behind authentication
+router.get('/blog/:id', withAuth, async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User
+        },
+        {
+          model: Channel,
+        },
+        {
+          model: Comment,
+        },
+      ],
+    });
+    const blog = blogData.get({ plain: true }); 
+    console.log(blog)
+    res.render('blog', {
+      blog,
+      logged_in: req.sessions.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err); 
+  }
+});
+
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     /* if (req.session.logged_in) {
